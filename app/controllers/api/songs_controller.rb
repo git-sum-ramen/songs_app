a431658da2a066a92911a539d321b723c64cede1
brian <riceb53@gmail.com>
2020-11-10 21:31:08 -0500

    songs update action

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -19,4 +19,14 @@ class Api::SongsController < ApplicationController
     @song.save
     render 'show.json.jb'
   end
+
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.title = params[:title]
+    @song.album = params[:album]
+    @song.year = params[:year]
+    @song.artist = params[:artist]
+    @song.save
+    render 'show.json.jb'
+  end
 end
